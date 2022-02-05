39c0181bea66b35065c9b38fa0b79d4b1330fe47
yonghyun123 <model9960@naver.com>
2022-02-05 16:20:55 +0900

    jpa 설정 완료

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +19,17 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception{
         Member member = new Member();
         member.setUsername("memberA");
         Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
 
-        Member member1 = memberRepository.find(savedId);
-        Assertions.assertThat(member1).isSameAs(member);
+        Assertions.assertThat(findMember).isSameAs(member);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(savedId);
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
