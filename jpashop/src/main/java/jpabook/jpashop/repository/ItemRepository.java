02dbfb99f2f1ff53bd6026653393c5c6f2a8fa38
yonghyun123 <model9960@naver.com>
2022-02-26 15:15:51 +0900

    상품 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -27,6 +27,6 @@ public class ItemRepository {
     }
 
     public List<Item> findAll() {
-        return em.createQuery("select i from item i", Item.class).getResultList();
+        return em.createQuery("select i from Item i", Item.class).getResultList();
     }
 }
---
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+    @Autowired private ItemRepository itemRepository;
+    @Autowired private ItemService itemService;
+    @Autowired private EntityManager em;
+
+    @Test  
+    public void 상품_등록(){
+        Item book = new Book();
+        book.setName("book");
+        book.setPrice(1000);
+        book.setStockQuantity(10);
+
+
+        //when
+        itemService.saveItem(book);
+
+        //then
+        em.flush();
+
+        Assertions.assertThat(book).isEqualTo(itemService.findOne(book.getId()));
+    }
+}
