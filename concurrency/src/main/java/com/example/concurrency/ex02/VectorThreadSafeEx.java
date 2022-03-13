fa5cf1169e3f965c60c781ce475f823c3aed6754
yonghyun123 <model9960@naver.com>
2022-03-13 18:33:09 +0900

    동시성 문제 ArrayList vs Vector

---
+++ concurrency/src/main/java/com/example/concurrency/ex02/VectorThreadSafeEx.java
@@ -0,0 +1,41 @@
+package com.example.concurrency.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class VectorThreadSafeEx {
+
+    static List<String> list=new ArrayList<>();
+    public static void main(String[] args){
+        list.add("reakwon");
+        list.add("hello");
+        list.add("world");
+
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                list.forEach((item)->{
+                    //1초마다 원소를 출력
+                    try {
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(item);
+                });
+            }
+        });
+
+        thread.start();	//thread 시작
+
+        //thread가 forEach문을 먼저 수행할 여유를 주기 위해 1초 기다림
+        try {
+            Thread.sleep(1000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        //thread가 forEach() 하는 중에 원소추가
+        list.add("thread-unsafe");
+    }
+}
