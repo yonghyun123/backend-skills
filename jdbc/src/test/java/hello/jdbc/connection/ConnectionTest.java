f96b96dcf8fb60a8f6e4bf7e67de82ae3524e62e
yonghyun123 <model9960@naver.com>
2022-06-05 15:46:46 +0900

    DataSource 예제 DriverManager

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,41 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection = {}, class = {} ", connection, connection.getClass());
+        log.info("connection2 = {}, class2= {} ", connection2, connection2.getClass());
+
+    }
+
+    @Test
+    void dataSourceDriverManger() throws SQLException {
+        //dataSourceDriverManger 항상 새로운 커넥션을 획득
+        DataSource driverManagerDataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        useDataSource(driverManagerDataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection conn1 = dataSource.getConnection();
+        Connection conn2 = dataSource.getConnection();
+
+        log.info("connection = {}, class = {} ", conn1, conn1.getClass());
+        log.info("connection2 = {}, class2= {} ", conn2, conn2.getClass());
+    }
+}
