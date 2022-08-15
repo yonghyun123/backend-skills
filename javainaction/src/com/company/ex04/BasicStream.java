f9389c1107fdc2990152ef868f2a01987e1f7ca3
yonghyun123 <model9960@naver.com>
2022-08-15 16:31:45 +0900

    Start jpa basic

---
+++ javainaction/src/com/company/ex04/BasicStream.java
@@ -0,0 +1,10 @@
+package com.company.ex04;
+
+import java.util.Arrays;
+
+public class BasicStream {
+
+    public static void main(String[] args) {
+        Arrays.asList("meal", "other", "cake", )
+    }
+}
