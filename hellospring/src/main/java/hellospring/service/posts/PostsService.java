c364040eaf522e3dd6c5039d6890e97700b51a9d
yonghyun123 <model9960@naver.com>
2022-11-06 17:47:38 +0900

    게시글 등록 조회 수정 기능 구현

--- hellospring/src/main/java/hellospring/service/posts/PostsService.java
+++ hellospring/src/main/java/hellospring/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package hellospring.service.posts;
 
+import hellospring.domain.posts.Posts;
 import hellospring.domain.posts.PostsRepository;
+import hellospring.web.dto.PostsResponseDto;
 import hellospring.web.dto.PostsSaveRequestDto;
+import hellospring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
+
     }
 }
--- hellospring/src/main/java/hellospring/web/PostsApiController.java
+++ hellospring/src/main/java/hellospring/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package hellospring.web;
 
 import hellospring.service.posts.PostsService;
+import hellospring.web.dto.PostsResponseDto;
 import hellospring.web.dto.PostsSaveRequestDto;
+import hellospring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,4 +18,13 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
