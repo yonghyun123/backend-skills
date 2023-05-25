bc02c937ebbaeec5fbf000f88c5a152aaf57d5d8
yonghyun123 <model9960@naver.com>
2023-05-25 09:29:36 +0900

    순수 JPA 레퍼지토리

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,10 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,5 +22,22 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 }
