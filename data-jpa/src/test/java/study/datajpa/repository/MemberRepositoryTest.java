9b1f10fd293d772fe21fb3e56bcb50b0ee84440a
yonghyun123 <model9960@naver.com>
2023-05-28 18:58:46 +0900

    @EntityGraph 배경

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -229,4 +229,18 @@ class MemberRepositoryTest {
         Assertions.assertThat(i).isEqualTo(4);
 
     }
+
+    @Test
+    public void findMemberLazy() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+    }
 }
