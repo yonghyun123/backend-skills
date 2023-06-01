20b881cb3b57098dc3c8fe1214e8faaf768ba1da
yonghyun123 <model9960@naver.com>
2023-06-01 23:01:54 +0900

    Web확장 - 페이징 정렬

--- data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -12,5 +13,11 @@ public class MemberDto {
         this.id = id;
         this.userName = userName;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.userName = member.getUserName();
+        this.teamName = member.getTeam().getName();
     }
 }
