19c66214586aa8351a48a40b15df261542d0efcd
yonghyun123 <model9960@naver.com>
2023-06-02 22:45:11 +0900

    새로운 엔티티 판단

---
+++ data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,15 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
